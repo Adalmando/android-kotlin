1744ffc8861acaf6d8b7f9aaf631c168f6a03acf
Adalmando da Silva Araújo <adalmando.silva@phoebus.local>
2023-07-12 17:41:18 -0300

    updating

--- AtividadeLinearLayout/app/src/main/java/com/adalmando/appgastos/MainActivity.kt
+++ AtividadeLinearLayout/app/src/main/java/com/adalmando/appgastos/MainActivity.kt
@@ -2,10 +2,34 @@ package com.adalmando.appgastos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.adalmando.appgastos.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View){
+        if(view.id == R.id.button_calculate){
+            calculate()
+        }
+    }
+    private fun calculate(){
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+        val total = (distance*price)/autonomy
+        // Toast notification:
+        //Toast.makeText(this, "O total gasto é: R$ ${"%.2f".format(total)}", Toast.LENGTH_SHORT).show()
+         binding.textTotalValue.text = "R$ ${"%.2f".format(total)}"
     }
 }
